c87576c987479baf58290d58145ba7aa6032e2f5
František Filip <frantisek.filip@plus4u.net>
2020-09-02 12:30:00 +0200

    client-detail works!

--- src/app/bussines-shared/bussines-shared.module.ts
+++ src/app/bussines-shared/bussines-shared.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ClientDetailComponent } from './client-detail/client-detail.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [ClientDetailComponent],
   imports: [
     CommonModule
+  ],
+  exports: [
+    ClientDetailComponent
   ]
 })
 export class BussinesSharedModule { }
