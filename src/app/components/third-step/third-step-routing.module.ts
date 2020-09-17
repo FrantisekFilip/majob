db21a1f891d129658a9d44d951f0ab91e5b3413e
Pavel Tetervak <pavel.tetervak@unicorn.com>
2020-09-17 19:40:27 +0200

    feature/NOJIRA fixed

--- src/app/components/third-step/third-step-routing.module.ts
+++ src/app/components/third-step/third-step-routing.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {Router, RouterModule, Routes} from '@angular/router';
-import {ThirdStepComponent} from "./third-step.component";
+import {ThirdStepComponent} from './third-step.component';
 
 const routes: Routes = [
   {
     path: '',
     component: ThirdStepComponent
   }
-]
+];
 
 @NgModule({
   declarations: [],
