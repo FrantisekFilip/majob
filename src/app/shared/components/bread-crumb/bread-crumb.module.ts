3e740ad222f8f08d70c8e7b8780b1f1a67620415
Pavel Flegr <pavelflegr@gmail.com>
2020-09-18 12:17:43 +0200

    bugfix/NOJIRA-fix spa link

--- src/app/shared/components/bread-crumb/bread-crumb.module.ts
+++ src/app/shared/components/bread-crumb/bread-crumb.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {BreadCrumbComponent} from './bread-crumb.component';
 import {TranslateModule} from "@ngx-translate/core";
+import { Routes, RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [BreadCrumbComponent],
   imports: [
     CommonModule,
-    TranslateModule
+    TranslateModule,
+    RouterModule
   ],
   exports: [BreadCrumbComponent]
 })
